c54bd173eac2f454bc8a55c0b52f99ff3d91ec62
SamTombs <samuel.tombs91@outlook.com>
2025-10-10 14:45:03 +0200

    Updated products serializer

--- products/serializers.py
+++ products/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from .models import Product
-
+from jwt_auth.serializers import UserSerializer
+from reviews.serializers.populated import PopulatedReviewSerializer
 
 class ProductSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Product
-    fields = '__all__'
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class PopulatedProductSerializer(ProductSerializer):
+    owner = UserSerializer()
+    populated_reviews = PopulatedReviewSerializer(many=True)
